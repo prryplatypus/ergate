1b13abca4ed8e147f4251da006dd22ac040e7148
Néstor Pérez <25409753+prryplatypus@users.noreply.github.com>
2024-06-28 23:48:35 +0200

    Make `WorkflowStep` take same args as function it wraps (#15)

--- ergate/workflow_step.py
+++ ergate/workflow_step.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from contextlib import ExitStack
-from typing import TYPE_CHECKING, Any, Callable
+from collections.abc import Generator
+from contextlib import ExitStack, contextmanager
+from typing import TYPE_CHECKING, Any, Callable, ParamSpec
 
 from .depends_cache import DependsCache
 from .inspect import build_function_arg_info
@@ -10,8 +11,15 @@ if TYPE_CHECKING:
     from .workflow import Workflow
 
 
+CallableSpec = ParamSpec("CallableSpec")
+
+
 class WorkflowStep:
-    def __init__(self, workflow: Workflow, callable: Callable[..., Any]) -> None:
+    def __init__(
+        self,
+        workflow: Workflow,
+        callable: Callable[CallableSpec, Any],
+    ) -> None:
         self.workflow = workflow
         self.callable = callable
         self.arg_info = build_function_arg_info(callable)
@@ -20,18 +28,20 @@ class WorkflowStep:
     def name(self) -> str:
         return self.callable.__name__
 
-    def __call__(self, last_return_value: Any, user_context: Any) -> Any:
+    @contextmanager
+    def build_args(
+        self, user_context: Any, last_return_value: Any
+    ) -> Generator[tuple[list[Any], dict[str, Any]], None, None]:
         with ExitStack() as stack:
-            args, kwargs = self.arg_info.build_args(
+            yield self.arg_info.build_args(
                 stack,
                 DependsCache(),
                 user_context,
                 last_return_value,
             )
 
-            result = self.callable(*args, **kwargs)
-
-        return result
+    def __call__(self, *args: CallableSpec.args, **kwargs: CallableSpec.kwargs) -> Any:
+        return self.callable(*args, **kwargs)
 
     def __str__(self) -> str:
         return f"{self.workflow.unique_name}.{self.callable.__name__}"
