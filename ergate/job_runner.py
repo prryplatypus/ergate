ac5f3f9893e4515ff692af39bf6374c10801af1a
Steven Gilroy <steven.gilroy@packetfabric.com>
2024-10-18 06:41:21 -0500

    `job_runner.py`: Add missing log output for `GoToEnd`.

--- ergate/job_runner.py
+++ ergate/job_runner.py
@@ -44,6 +44,7 @@ class JobRunner(Generic[JobType]):
             job.mark_aborted(exc.message)
         except GoToEnd as exc:
             job.mark_step_n_completed(len(workflow), exc.retval, len(workflow))
+            LOG.info(f"User requested to go to end.")
         except GoToStep as exc:
             if not exc.has_step:
                 err = "No label or index provided for GoToStep."
