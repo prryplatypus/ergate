bae01e776e895d7b89674e9f53cd7106d2dfcca5
Steven Gilroy <steven.gilroy@packetfabric.com>
2024-10-21 13:48:07 -0500

    `job_runner.py`: Add debug code.

--- ergate/job_runner.py
+++ ergate/job_runner.py
@@ -31,6 +31,9 @@ class JobRunner(Generic[JobType]):
         workflow = self.workflow_registry[job.workflow_name]
         step_to_run = workflow[job.current_step]
         paths = workflow.calculate_paths(job.current_step)
+        print("===311.1===", len(paths))
+        for i, l in enumerate(paths):
+            print(f"===311.2.{i}===", l)
 
         job.mark_running(step_to_run)
         self.state_store.update(job)
@@ -44,6 +47,7 @@ class JobRunner(Generic[JobType]):
             LOG.info("User requested to abort job: %s", exc)
             job.mark_aborted(exc.message)
         except GoToEnd as exc:
+            print("===311.3===", exc, job.current_step + 1, job.current_step + 1)
             job.mark_step_n_completed(job.current_step + 1, exc.retval, job.current_step + 1)
             LOG.info(
                 "User requested to go to end of workflow - return value: %s", exc.retval
@@ -70,11 +74,17 @@ class JobRunner(Generic[JobType]):
                 )
 
             remaining_steps = max(map(len, filter(lambda steps: isinstance(steps[0][1], GoToStep) and steps[0][1].next_step == exc.next_step, paths)), default=0)
+
+            print("===311.4===", exc, remaining_steps, job.current_step + 1 + remaining_steps)
+
             job.mark_step_n_completed(idx, exc.retval, job.current_step + 1 + remaining_steps)
         except SkipNSteps as exc:
             LOG.info("User requested to skip %d steps", exc.n)
 
             remaining_steps = max(map(len, filter(lambda steps: isinstance(steps[0][1], SkipNSteps) and steps[0][1].n == exc.n, paths)), default=0)
+
+            print("===311.5===", exc, exc.n + 1, job.steps_completed + 1 + remaining_steps)
+
             job.mark_n_steps_completed(exc.n + 1, exc.retval, job.steps_completed + 1 + remaining_steps)
         except Exception as exc:
             LOG.exception("Job raised an exception")
@@ -83,6 +93,9 @@ class JobRunner(Generic[JobType]):
         else:
             LOG.info("Step completed successfully - return value: %s", retval)
             remaining_steps = max(map(len, filter(lambda steps: steps[0][1] is None)), default=0)
+
+            print("===311.6===", None, 1, job.steps_completed + 1 + remaining_steps)
+
             job.mark_n_steps_completed(1, retval, job.steps_completed + 1 + remaining_steps)
 
         self.state_store.update(job)
