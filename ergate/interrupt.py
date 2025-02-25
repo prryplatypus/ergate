211b4e8a300b46d453daf8f220e708a0e91d3fa6
Néstor Pérez <25409753+prryplatypus@users.noreply.github.com>
2025-02-25 20:13:38 +1100

    Add caution regarding breaking changes (#24)

--- ergate/interrupt.py
+++ ergate/interrupt.py
@@ -1,12 +1,17 @@
 import signal
+from types import FrameType
+from typing import Any, Callable
 
 from .log import LOG
+
+# https://github.com/python/typeshed/blob/af32625bd489f1f277ed984543bb87408b09d824/stdlib/signal.pyi#L68
+_SIGNAL_HANDLER = Callable[[int, FrameType | None], Any] | int | signal.Handlers | None
 
 
 class DelayedKeyboardInterrupt:
     def __enter__(self) -> None:
         self.signals_received: list[tuple] = []
-        self.old_handlers = {
+        self.old_handlers: dict[signal.Signals, _SIGNAL_HANDLER] = {
             signal.SIGTERM: signal.signal(signal.SIGTERM, self.handler),
             signal.SIGINT: signal.signal(signal.SIGINT, self.handler),
         }
