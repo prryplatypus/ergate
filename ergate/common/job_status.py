8c7d0a988b40bb34a3ed414735e282e8df7f6867
prryplatypus <25409753+prryplatypus@users.noreply.github.com>
2025-02-27 16:24:38 +1100

    Update job status docstrings

--- ergate/common/job_status.py
+++ ergate/common/job_status.py
@@ -2,28 +2,26 @@ from enum import IntEnum, auto
 
 
 class JobStatus(IntEnum):
-    ...  # Just here so the formatter doesn't think first string is docstring
-
-    """Job has not been queued yet"""
     PENDING = auto()
+    """Job has not been queued yet"""
 
-    """Job is in the queue, awaiting processing"""
     QUEUED = auto()
+    """Job is in the queue, awaiting processing"""
 
-    """Job has been taken by a worker and is currently running"""
     RUNNING = auto()
+    """Job has been taken by a worker and is currently running"""
 
-    """Job has been completed successfully - no further steps"""
     COMPLETED = auto()
+    """Job has been completed successfully - no further steps"""
 
-    """An exception has been raised from one of the job's steps"""
     FAILED = auto()
+    """An exception has been raised from one of the job's steps"""
 
-    """Job has been aborted from within a step - execution stopped"""
     ABORTED = auto()
+    """Job has been aborted from within a step - execution stopped"""
 
-    """Job will be cancelled after the current step completes"""
     CANCELLING = auto()
+    """Job will be cancelled after the current step completes"""
 
-    """Job has been cancelled - no further steps"""
     CANCELLED = auto()
+    """Job has been cancelled - no further steps"""
